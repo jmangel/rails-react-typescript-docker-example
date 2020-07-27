93d5499a1590e93406aca73284e1625acf1cf100
John Mangel <john.mangel@appfolio.com>
2020-07-26 19:42:43 -0700

    jm - add root scale to modes

--- frontend/src/__tests__/ChordMapper.test.tsx
+++ frontend/src/__tests__/ChordMapper.test.tsx
@@ -9,20 +9,83 @@ it('gets double sharp', () => {
 });
 
 it('gives dorian and aeolian for minor', () => {
-  expect(
-    scalesForChord('A', '-')
-  ).toEqual([
-    {
-      scaleName: 'aeolian',
-      scaleNotes: [
-        'A','B','C','D','E','F','G'
-      ]
-    },
-    {
-      scaleName: 'dorian',
-      scaleNotes: [
-        'A','B','C','D','E','F#','G'
-      ]
-    },
+  const minorScales = scalesForChord('A', '-')
+
+  const aeolianScale = minorScales[0]
+  const dorianScale = minorScales[1]
+
+  expect(aeolianScale.scaleName).toEqual('aeolian')
+  expect(aeolianScale.scaleNotes).toEqual([
+    'A','B','C','D','E','F','G'
+  ])
+
+  expect(dorianScale.scaleName).toEqual('dorian')
+  expect(dorianScale.scaleNotes).toEqual([
+    'A','B','C','D','E','F#','G'
   ])
 });
+
+describe('rootScale', () => {
+  it('returns major for major modes', () => {
+    const majorScales = scalesForChord('A', '^')
+    const minorScales = scalesForChord('A', '-')
+
+    const ionianScale = majorScales[0]
+
+    const aeolianScale = minorScales[0]
+    const dorianScale = minorScales[1]
+
+    expect(ionianScale.rootScale).toEqual('major')
+    expect(aeolianScale.rootScale).toEqual('major')
+    expect(dorianScale.rootScale).toEqual('major')
+  })
+
+  // TODO: 7b9 requires diminished primary scale, not yet implemented
+  // it('returns melodic minor for melodic minor modes', () => {
+  //   const dominantFlatNineScales = scalesForChord('A', '7b9')
+
+  //   const phrygianDominantScale = dominantFlatNineScales[0]
+  //   expect(phrygianDominantScale.rootScale).toEqual('melodic minor')
+  // })
+
+  it('returns harmonic minor for harmonic minor modes', () => {
+    const majorScales = scalesForChord('A', '^')
+
+    const lydianSharpTwoScale = majorScales[1]
+    expect(lydianSharpTwoScale.rootScale).toEqual('harmonic minor')
+  })
+
+  // TODO: 7b9 requires diminished primary scale, not yet implemented
+  // it('returns diminished for diminished modes', () => {
+  //   const dominantFlatNineScales = scalesForChord('A', '7b9')
+
+  //   const halfWholeDiminishedScale = dominantFlatNineScales[1]
+  //   expect(halfWholeDiminishedScale.rootScale).toEqual('diminished')
+  // })
+})
+
+describe('rootScaleNote', () => {
+  it('returns first for major modes', () => {
+    expect(scalesForChord('C', '^')[0].rootScaleNote).toEqual('C')
+    expect(scalesForChord('F#', '^')[0].rootScaleNote).toEqual('F#')
+    expect(scalesForChord('Db', '^')[0].rootScaleNote).toEqual('Db')
+  })
+
+  it('returns seventh for dorian modes', () => {
+    expect(scalesForChord('D', '-')[1].rootScaleNote).toEqual('C')
+    expect(scalesForChord('G#', '-')[1].rootScaleNote).toEqual('F#')
+    expect(scalesForChord('Eb', '-')[1].rootScaleNote).toEqual('Db')
+  })
+
+  it('returns third for aeolian modes', () => {
+    expect(scalesForChord('A', '-')[0].rootScaleNote).toEqual('C')
+    expect(scalesForChord('D#', '-')[0].rootScaleNote).toEqual('F#')
+    expect(scalesForChord('Bb', '-')[0].rootScaleNote).toEqual('Db')
+  })
+
+  it('returns third for lydian #2 modes', () => {
+    expect(scalesForChord('Ab', '^')[1].rootScaleNote).toEqual('C')
+    expect(scalesForChord('D', '^')[1].rootScaleNote).toEqual('F#')
+    expect(scalesForChord('Bbb', '^')[1].rootScaleNote).toEqual('Db')
+  })
+})
