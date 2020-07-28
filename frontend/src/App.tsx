b5e48599bd47e4a5b20ff5875e3614d167cb9659
John Mangel <john.mangel@appfolio.com>
2020-07-27 21:39:57 -0700

    jm - allow adding multiple rows at a time

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col, FormGroup, Label, Input } from 'reactstrap';
+import { Container, Button, Card, CardBody, CardText, Input } from 'reactstrap';
 import './App.css';
 import ChordRow from './ChordRow'
 
@@ -20,6 +20,7 @@ const App: React.FC = () => {
   const [content, updateContent] = React.useState('Waiting for a response from Rails...');
 
   const [chordRows, setChordRows] = React.useState(1);
+  const [newChordRows, setNewChordRows] = React.useState(1);
 
   React.useEffect(() => {
     fetchContent(updateContent);
@@ -39,9 +40,22 @@ const App: React.FC = () => {
       <Container>
         {[...Array(chordRows)].map(() => <ChordRow></ChordRow>)}
       </Container>
-      <button onClick={() => setChordRows(chordRows + 1)}>
-        Add a Row
-      </button>
+      <Card className='w-25 mx-auto'>
+        <CardBody>
+          <CardText>
+            Add
+            <Input
+              type="number"
+              name="newChordRows"
+              value={newChordRows}
+              onChange={e => setNewChordRows(parseInt(e.target.value))}
+              className='w-25 mx-auto'
+            />
+            Row(s)
+          </CardText>
+          <Button onClick={() => setChordRows(chordRows + newChordRows)}>Add</Button>
+        </CardBody>
+      </Card>
     </div>
   );
 }
