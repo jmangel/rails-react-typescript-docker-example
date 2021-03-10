c9aab08064672f2863cad90d2f4d8f5bd88e9e6b
John Mangel <john.mangel@appfolio.com>
2021-03-10 11:49:24 -0800

    start splitting measure into chords in RawIRealParser

--- frontend/src/RawIRealParser.tsx
+++ frontend/src/RawIRealParser.tsx
@@ -12,11 +12,23 @@ interface ParsedMeasure {
 const basicMeasureSplitter = String.raw ` ?L?Z|[\|\]\}]`;
 const measureSplitterRegexWithXyQ = new RegExp(`(?<=XyQ(?!${basicMeasureSplitter})|${basicMeasureSplitter})`);
 
+const noteRegex = /([ABCDEFG][#b]?)/;
+const degreeRegex = /[56791113]/;
+const qualityRegex = new RegExp(`()|((([-+^ho]|add)*${degreeRegex}?([#b]?${degreeRegex})*sus?)alt?)`)
+// const quality =  ('*' [^*]* '*' ) / (( ( [-+^ho] / 'add')* <degree>? ( [#b]? <degree> )*  'sus'? )  'alt'?)
+// const rootnote = '/' <note>
+
+const chordSplitter = /[ ,]/;
+const chordMatcher = /[A-Gb#-^h/]*/;
+
 export const rawToMeasures = (raw: string): ParsedMeasure[] => {
   console.warn(raw);
   raw = raw.trim();
 
-  const measures = raw.split(measureSplitterRegexWithXyQ).filter((measure) => measure !== '').map((measureString) => {
+  const splitMeasures = raw.split(measureSplitterRegexWithXyQ).filter((measure) => measure !== '');
+  console.warn(splitMeasures);
+
+  const measures = splitMeasures.map((measureString) => {
     return {
       chords: [{
         note: measureString
@@ -24,7 +36,7 @@ export const rawToMeasures = (raw: string): ParsedMeasure[] => {
     };
   });
 
-  console.warn(measures);
+  // console.warn(measures);
 
   return measures;
 }
