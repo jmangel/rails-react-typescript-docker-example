4d94c652b1c7dc660b157db2f0cf2971e23c6f0f
John Mangel <john.mangel@appfolio.com>
2021-03-12 15:12:17 -0800

    copy manifest.json to dist/

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,5 +1,15 @@
 const path = require("path");
 const HTMLPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+// function modify(buffer) {
+//   // copy-webpack-plugin passes a buffer
+//   var manifest = JSON.parse(buffer.toString());
+
+//   // pretty print to JSON with two spaces
+//   manifest_JSON = JSON.stringify(manifest, null, 2);
+//   return manifest_JSON;
+// }
 
 module.exports = {
   module: {
@@ -29,6 +39,20 @@ module.exports = {
   plugins: [
     new HTMLPlugin({
       template: path.join(__dirname, "src/index.html"),
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "./static/manifest.json",
+          to:   "./static/manifest.json",
+          // transform (content, _) {
+          //     return modify(content)
+          // }
+        },
+      ],
+      options: {
+        concurrency: 100,
+      },
     }),
   ],
   devServer: {
